be55b52cd36b1d00474b059ebc27c309e901f89a
Vasanth2801 <vasanthravichandran28@gmail.com>
2025-12-18 13:05:08 +0530

    UI,Turn based logic and hints fir the player

---
+++ Chess 2D/Assets/Scripts/MoveCircle.cs
@@ -0,0 +1,73 @@
+using UnityEngine;
+
+public class MoveCircle : MonoBehaviour
+{
+    public GameObject controller;
+
+    
+    GameObject reference = null;
+
+    int matrixX;
+    int matrixY;
+
+   
+    public bool attack = false;
+
+    public void Start()
+    {
+        if (attack)
+        {
+            
+            gameObject.GetComponent<SpriteRenderer>().color = new Color(1.0f, 0.0f, 0.0f, 1.0f);
+        }
+    }
+
+    public void OnMouseUp()
+    {
+        controller = GameObject.FindGameObjectWithTag("Game");
+
+        
+        if (attack)
+        {
+            GameObject cp = controller.GetComponent<GameController>().GetPosition(matrixX, matrixY);
+
+            if (cp.name == "white_king") controller.GetComponent<GameController>().Winner("black");
+            if (cp.name == "black_king") controller.GetComponent<GameController>().Winner("white");
+
+            Destroy(cp);
+        }
+
+        
+        controller.GetComponent<GameController>().SetPositionEmpty(reference.GetComponent<Chess>().GetXBoard(),
+            reference.GetComponent<Chess>().GetYBoard());
+
+        reference.GetComponent<Chess>().SetXBoard(matrixX);
+        reference.GetComponent<Chess>().SetYBoard(matrixY);
+        reference.GetComponent<Chess>().SetCoords();
+
+        
+        controller.GetComponent<GameController>().SetPosition(reference);
+
+        
+        controller.GetComponent<GameController>().NextTurn();
+
+        
+        reference.GetComponent<Chess>().DestroyMovePlates();
+    }
+
+    public void SetCoords(int x, int y)
+    {
+        matrixX = x;
+        matrixY = y;
+    }
+
+    public void SetReference(GameObject obj)
+    {
+        reference = obj;
+    }
+
+    public GameObject GetReference()
+    {
+        return reference;
+    }
+}
